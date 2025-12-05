33219cff81d917a5e84cfddc917a64a4d1b929b4
Viktor31642 <viktor31642@gmail.com>
2025-12-05 17:05:53 +0200

    add users

---
+++ Users.cs
@@ -0,0 +1,34 @@
+﻿public class BaseUser
+{
+    private string secretCode;
+    protected string role;
+
+    public void SetRole(string r) {  role = r; }
+    public void ShowRole() { Console.WriteLine(role); }
+    public void SetSecretCode(string code) { secretCode = code; }
+    public void ShowSecretCode() 
+    { 
+        if (secretCode == null) { Console.WriteLine("No secret code set"); }
+
+        else { Console.WriteLine("Secret code length: " + secretCode.Length); }
+
+    }
+
+}
+
+public class AdminUser : BaseUser
+{
+    public void PrintAdminInfo()
+    {
+        Console.WriteLine("Admin user active");
+     
+    }
+}
+
+public class GuestUser : BaseUser 
+{
+    public void PrintGuestInfo()
+    {
+        Console.WriteLine("Guest user active");
+    }
+}
